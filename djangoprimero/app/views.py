bc0deaf9dfc3f92d6367551caec86a123e57b6d2
SebaChuaqui <schuaquicano@gmail.com>
2021-07-29 21:25:29 -0400

    Primer Django Final

--- djangoprimero/app/views.py
+++ djangoprimero/app/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import redirect, render
-
-# Create your views here.
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 
 def root(request):
     return redirect("/blogs")
 def index(request):
-    return HttpResponse(f'<h1>Mostrar una lista de todos los blogs<h2>')
+    return HttpResponse(f'<h1>Una lista de todos los blogs<h2>')
 def new(request):
-    return HttpResponse(f'<p>Mostrar un nuevo formulario para crear un nuevo blog<p>')
+    return HttpResponse(f'<p>Nuevo formulario para crear un nuevo blog<p>')
 def create(request):
     return redirect('/')
 def show(request, number):
-    return HttpResponse(f'<h3>para mostrar el blog numero {number}</h3>')
+    return HttpResponse(f'<h3>Mostrar el blog numero {number}</h3>')
 def edit(request, number):
     return HttpResponse(f'<h4>Editar el blog {number}</h1>')
 def destroy(request, number):
